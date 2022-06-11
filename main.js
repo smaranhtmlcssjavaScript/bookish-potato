58a99a22ac4a4b3d753b850da71c2ee53e160f93
smaranhtmlcssjavaScript <78704480+smaranhtmlcssjavaScript@users.noreply.github.com>
2022-06-10 21:09:57 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+status = "";
+function preload() {
+}
+function setup() {
+    canvas = createCanvas(480,380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(480,380);
+    video.hide();
+}
+function draw() {
+    image(video, 0, 0, 480, 380);
+}
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+function modelLoaded() {
+    console.log("model is loaded");
+    status = true;
+}
